7c13b096f76572389da18feb367b8b62b88bb6c2
Eng-ahmed <elkossairyahmed87@gmail.com>
2026-07-18 18:35:42 +0300

    finish

--- users/admin.py
+++ users/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Users, Address, Doctors, Patients, Specialty
 
-admin.site.register(Users)
 admin.site.register(Address)
+admin.site.register(Users)
 admin.site.register(Doctors)
 admin.site.register(Patients)
 admin.site.register(Specialty)
